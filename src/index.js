2df0bb9fc9cd19bb49c9ca771b7e8b0963e5730c
Gonzalo Piffaretti <gpiffaretti@gmail.com>
2020-09-07 21:32:20 +0200

    fix: Added pull again to get latest tags

--- src/index.js
+++ src/index.js
@@ -63,8 +63,8 @@ async function run() {
     core.info(`Skipping empty releases is "${skipEmptyRelease ? 'enabled' : 'disabled'}"`)
     core.info(`Skipping the update of the version file is "${skipVersionFile ? 'enabled' : 'disabled'}"`)
 
-    //core.info('Pull to make sure we have the full git history')
-    //await git.pull()
+    core.info('Pull to make sure we have the full git history')
+    await git.pull()
 
     const config = conventionalConfigFile && require(resolve(process.cwd(), conventionalConfigFile));
 
